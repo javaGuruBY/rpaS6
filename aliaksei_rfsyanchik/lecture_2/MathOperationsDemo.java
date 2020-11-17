b68ce4890c642ea5096084124bb20bbb99985142
kasyanl <74323441+kasyanl@users.noreply.github.com>
2020-11-17 08:55:06 +0300

    Create MathOperationsDemo.java

---
+++ aliaksei_rfsyanchik/lecture_2/MathOperationsDemo.java
@@ -0,0 +1,45 @@
+package home;
+
+public class MathOperationsDemo {
+	public static void main (String args[] ) {
+		System.out.println("Welcome to the 'Math Operations'!");
+		int a = 100;
+		 int b = 45;
+		 System.out.println("1) Addition '+':"); 
+		 System.out.print("a = " +a+ ", b = " +b); 
+		 
+		 System.out.println(); 
+		 System.out.print("a + b = " +a+ " + " +b+  " = " +(a+b)); 
+		 
+		 System.out.println(); 
+		 System.out.println(); 
+		 int c = 53;
+		 int d = 17;
+		 System.out.println("2) Subtraction '-':"); 
+		 System.out.print("c = " +c+ ", d = " +d); 
+		 
+		 System.out.println(); 
+		 System.out.print("c - d = " +c+ " - " +d+  " = " +(c-d)); 
+		 
+		 System.out.println(); 
+		 System.out.println(); 
+		 double e = 25;
+		 double f = 3;
+		 System.out.println("3) Division '/':"); 
+		 System.out.print("e = " +e+ ", f = " +f); 
+		 
+		 System.out.println(); 
+		 System.out.print("e / f = " +e+ " / " +f+  " = " +(e/f)); 
+
+		 System.out.println(); 
+		 System.out.println(); 
+		 int g = 9;
+		 int h = 3;
+		 System.out.println("4) Multiplication '*':"); 
+		 System.out.print("g = " +g+ ", h = " +h); 
+		 
+		 System.out.println(); 
+		 System.out.print("g * h = " +g+ " * " +h+  " = " +(g*h)); 
+	}
+
+}
