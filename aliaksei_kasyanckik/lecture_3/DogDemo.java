e4731134bfe5a30bde325a0ba688c093a7f08a36
kasyanl <74323441+kasyanl@users.noreply.github.com>
2020-11-17 14:05:59 +0300

    Create DogDemo.java

---
+++ aliaksei_kasyanckik/lecture_3/DogDemo.java
@@ -0,0 +1,22 @@
+package home;
+
+public class DogDemo {
+	 public static void main(String[] args) {
+		 Dog dog = new Dog(5, "black", "Rokki");
+		
+		 System.out.println("Dog age = " + dog.age);
+		 System.out.println("Dog color = " + dog.color);
+		 System.out.println("Dog name = " + dog.name);
+
+		 System.out.println();
+		 System.out.println("Dog voice = " + dog.isOn);
+		 System.out.println("Dog eat = " + dog.eaOn);
+		 System.out.println("Dog sleep = " + dog.slOn);
+		 
+		 System.out.println();
+		 System.out.println("Dog in sleep!");
+		 dog.sleepOn();
+		 System.out.println("Dog sleep = " + dog.slOn);
+		
+	}
+}
