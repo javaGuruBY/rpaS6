bbc18496bb02ab7f7d0644b3deaa5908e7d5868e
Yury Bialkevich <darkerjura@gmail.com>
2020-11-19 22:36:56 +0300

    general math operation

---
+++ yury_belkevich/lecture2/hw3/src/main/java/by/jrr/start/MathOperationsDemo.java
@@ -0,0 +1,10 @@
+package by.jrr.start;
+
+class MathOperationDemo {
+    public static void main(String[] args) {
+        System.out.println(2 + 1);
+        System.out.println(2 - 1);
+        System.out.println(2 * 2);
+        System.out.println(4 / 2);
+    }
+}
