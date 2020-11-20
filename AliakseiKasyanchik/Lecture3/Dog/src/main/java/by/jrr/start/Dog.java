186fd495bfabcddea906a79bda439418473d2eb2
Kasyanl <1784158@tut.by>
2020-11-20 13:33:58 +0300

    DogDemo

---
+++ AliakseiKasyanchik/Lecture3/Dog/src/main/java/by/jrr/start/Dog.java
@@ -0,0 +1,34 @@
+package by.jrr.start;
+
+public class Dog {
+    int age;
+    String color;
+    String name;
+
+    public Dog(int age, String color, String name) {
+        this.age = age;
+        this.color = color;
+        this.name = name;
+    }
+    public boolean isOn;
+    public void voiceOn() {
+        this.isOn = true;
+    }
+    public void voiceOff() {
+        this.isOn = false;
+    }
+    public boolean eaOn;
+    public void eatOn() {
+        this.eaOn = true;
+    }
+    public void eatOff() {
+        this.eaOn = false;
+    }
+    public boolean slOn;
+    public void sleepOn() {
+        this.slOn = true;
+    }
+    public void sleepOff() {
+        this.slOn = false;
+    }
+}
