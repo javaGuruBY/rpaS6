64c4dd864486636e479b4108487bd546baa6ecee
Kasyanl <1784158@tut.by>
2020-11-20 12:28:36 +0300

    hw1

---
+++ AliakseiKasyanchik/Lecture1/src/main/java/by/jrr/start/hw1t2.java
@@ -0,0 +1,70 @@
+package by.jrr.start;
+
+public class hw1t2 {
+    public static void main(String[] args) {
+
+        System.out.println("Начало движения автомобиля");
+        System.out.println("1. подойти к автомобилю");
+        System.out.println("2. открыть автомобиль");
+        System.out.println("3. сесть в автомобиль");
+        System.out.println("4. завести двигатель");
+        System.out.println("5. если двигатель не завелся, то конец");
+        System.out.println("6. включить передачу");
+        System.out.println("7. начать жвиженея");
+        System.out.println();
+
+        System.out.println("Заправка автомобиля топливом");
+        System.out.println("1. подъехать на АЗС");
+        System.out.println("2. заглушить двигатель");
+        System.out.println("3. выйти из автомобиля");
+        System.out.println("4. открыть лючок бензобака");
+        System.out.println("5. вставить заправочный пистолет в бак");
+        System.out.println("6. подойти к кассе");
+        System.out.println("7. назвать номер колонки и количество топлива");
+        System.out.println("8. произвести оплату (огорчившись от стоимости топлива)");
+        System.out.println("9. если денег нет, то к пункту 12");
+        System.out.println("10. подойти к автомобилю");
+        System.out.println("11. дождаться завершения заправки");
+        System.out.println("12. достать заправочный пистолет из бака");
+        System.out.println("13. закрыть лючок бензобака");
+        System.out.println("14. сесть в автомобиль");
+        System.out.println("15. завести двигатель");
+        System.out.println("16. начать движение");
+        System.out.println();
+        System.out.println("Достать любимое варенье");
+        System.out.println("1. подойти к подвалу");
+        System.out.println("2. открыть подвал");
+        System.out.println("3. включить свет");
+        System.out.println("4. спуститься в подвал");
+        System.out.println("5. найти любимое варенье");
+        System.out.println("6. если любимого варенья нет, то к пункту 8");
+        System.out.println("7. взять банку варенья");
+        System.out.println("8. подняться из подвала");
+        System.out.println("9. выключить свет");
+        System.out.println("10. закрыть подвал");
+        System.out.println();
+        System.out.println("Нагреть воду");
+        System.out.println("1. взять чайник");
+        System.out.println("2. если вода в чайнике горячая, то конец");
+        System.out.println("3. если вода в чайнике есть, то к пункту 6");
+        System.out.println("4. подойти к раковине");
+        System.out.println("5. налить воды");
+        System.out.println("6. поставить чайник на подставку");
+        System.out.println("7. включить чайник");
+        System.out.println("8 дождаться, когда чайник закипит");
+        System.out.println();
+        System.out.println("Накачка автомобильной шины ручным насосом");
+        System.out.println("1. подойти к спущенному колесу");
+        System.out.println("2. открутить защитный колпачок");
+        System.out.println("3. взять насос");
+        System.out.println("4. подсоединить насос");
+        System.out.println("5. накачать шину");
+        System.out.println("6. отсоединить насос");
+        System.out.println("7. взять манометр");
+        System.out.println("8. проверить давление");
+        System.out.println("9. если давление ниже требуемого, то к к пункту 1");
+        System.out.println("10. закрутить защитный колпачек");
+
+    }
+
+}
