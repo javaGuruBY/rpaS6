2728d98c77784f9ec2416a04180003dafb01ec78
Kasyanl <1784158@tut.by>
2020-11-20 13:40:10 +0300

    CircleDemo

---
+++ AliakseiKasyanchik/Lecture3/Circle/src/main/java/by/jrr/start/Circle.java
@@ -0,0 +1,9 @@
+package by.jrr.start;
+
+public class Circle {
+    public double radius;
+    public double calculateArea() {
+        return (3.14 * (this.radius * this.radius));
+
+    }
+}
---
+++ AliakseiKasyanchik/Lecture3/Circle/src/main/java/by/jrr/start/CircleDemo.java
@@ -0,0 +1,16 @@
+package by.jrr.start;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+
+        circle.radius = 2.35;
+        double p1 = circle.calculateArea();
+        System.out.println("Area circle side radius is " + circle.radius + " = " + p1);
+
+        circle.radius = 7.16;
+        double radius2 = circle.calculateArea();
+        System.out.println("Area circle side radius is " + circle.radius + " = " + radius2);
+    }
+}
