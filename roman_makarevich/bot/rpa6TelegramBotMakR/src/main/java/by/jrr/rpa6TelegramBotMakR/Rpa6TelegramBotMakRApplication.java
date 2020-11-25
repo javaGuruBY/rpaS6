79b6e3fc351123f11a21832774938b75af048f3e
mak <romanmak955@gmail.com>
2020-11-25 13:21:14 +0300

    init commit

---
+++ roman_makarevich/bot/rpa6TelegramBotMakR/src/main/java/by/jrr/rpa6TelegramBotMakR/Rpa6TelegramBotMakRApplication.java
@@ -0,0 +1,13 @@
+package by.jrr.rpa6TelegramBotMakR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rpa6TelegramBotMakRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rpa6TelegramBotMakRApplication.class, args);
+	}
+
+}
---
+++ roman_makarevich/bot/rpa6TelegramBotMakR/src/test/java/by/jrr/rpa6TelegramBotMakR/Rpa6TelegramBotMakRApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.rpa6TelegramBotMakR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rpa6TelegramBotMakRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
